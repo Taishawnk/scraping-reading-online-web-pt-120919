b49eb3521e7cdcb7e3189af63bead70f63f2cc25
Taishawn King <taishawnking1@yahoo.com>
2020-02-06 04:46:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,12 @@
 require 'nokogiri'
 require 'open-uri'
 
+<<<<<<< HEAD
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 p doc.css("a.link-3R0TyF")[0].name 
+=======
+p doc.css("headline-26OIBN").text 
+>>>>>>> 36736ba08040cc550ed58eb9bb1ab3242994f268
 
 
 
